b8be4bbda0418cbee12cfd5b09f4fa1c28543b5c
Pankaj1105 <98206434+Pankaj1105@users.noreply.github.com>
2023-04-24 20:53:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
-    //Write your code here
+  const firstName = document.querySelector('input[name="fname"]').value;
+  const lastName = document.querySelector('input[name="lname"]').value;
 
+  alert(`${firstName}${lastName}`);
 }
